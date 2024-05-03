5a2c411a03ab98b5112e721ea7fa3d4ddd129c38
Kishan Gupta <kishangupta4514@gmail.com>
2024-05-03 18:10:27 +0530

    change in layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
+  title: "Kishan's Portolio",
   description: "Kishan Gupta",
 };
 

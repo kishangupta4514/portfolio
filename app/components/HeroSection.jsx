65daa4440f8495733581e3513de0bc731af5a699
Kishan Gupta <kishangupta4514@gmail.com>
2024-05-16 17:48:44 +0530

    Update in Resume

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -38,7 +38,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <a
-              href="Kishan's Resume.pdf"
+              href="Kishan's UP Resume.pdf"
               download="kishan"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
